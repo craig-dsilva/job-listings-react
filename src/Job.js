4fcf2baee3f72e0262165f55368bd23047e9b2ef
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-04-16 11:23:51 +0100

    Mobile design complete

--- src/Job.js
+++ src/Job.js
@@ -1,20 +1,41 @@
 import React from "react";
 
 const Job = ({ data }) => {
-  console.log(data.logo);
   return (
-    <div className="job">
-      <img src={data.logo} alt={data.company} />
-      <h3>{data.company}</h3>
-      {data.new ? <p>New</p> : ""}
-      {data.featured ? <p>Featured</p> : ""}
-      <h4>{data.position}</h4>
-      <p>{data.postedAt}</p>
-      <p>{data.contract}</p>
-      <p>{data.location}</p>
+    <div className={`job ${data.featured ? "featured-border" : ""}`}>
+      <div className="job-info">
+        <img className="job-logo" src={data.logo} alt={data.company} />
+        <h3 className="job-company">{data.company}</h3>
+        <div className="job-keywords">
+          {data.new ? <p className="new-keyword">New</p> : ""}
+          {data.featured ? <p className="featured-keyword">Featured</p> : ""}
+        </div>
+        <h4 className="job-position">{data.position}</h4>
+        <div className="job-description">
+          <p>{data.postedAt}</p>
+          <p>{data.contract}</p>
+          <p>{data.location}</p>
+        </div>
+      </div>
       <hr />
-      <p>{data.role}</p>
-      <p>{data.level}</p>
+      <div className="job-tags">
+        <div className="job-tag">
+          <p>{data.role}</p>
+        </div>
+        <div className="job-tag">
+          <p>{data.level}</p>
+        </div>
+        {data.languages.map((language, index) => (
+          <div className="job-tag" key={index}>
+            <p>{language}</p>
+          </div>
+        ))}
+        {data.tools.map((tool, index) => (
+          <div className="job-tag" key={index}>
+            <p>{tool}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
