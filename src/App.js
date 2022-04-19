4eeca88fe4d766b23bad70f92c32ec2910026c16
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-04-19 18:00:23 +0100

    Filtering partially complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-import Job from "./Job";
+import Filters from "./Filters";
 
 import data from "./data/data.json";
 
 import "./App.css";
+import Jobs from "./Components/Jobs/Jobs";
 
 function App() {
+  const [jobs, setJobs] = useState(data);
+  const [keywords, setKeywords] = useState([]);
+
+  const addFilter = () => {
+    const copyOfKeywords = [...keywords];
+    // Push
+    setKeywords(copyOfKeywords);
+    setJobs(
+      data.filter((job) => {
+        return keywords.includes(job.role) || keywords.includes(job.level);
+      })
+    );
+  };
+
+  console.log(jobs);
+
   return (
     <div className="App">
       <header className="header"></header>
-      <main className="main">
-        {data.map((job) => {
-          return <Job key={job.id} data={job} />;
-        })}
-      </main>
+      <Filters />
+      <Jobs jobs={jobs} />
     </div>
   );
 }
